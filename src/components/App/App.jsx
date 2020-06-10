21f8beb155ce145d2a6a12dc2293fac7a391ce9b
Juan Carlos GR <c.ramirez@globant.com>
2020-06-09 20:03:41 -0500

    [HE-003] Redux config

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import React, { PureComponent } from "react";
+
+/**
+ * Root component of the tree.
+ */
+class App extends PureComponent {
+  render = () => {
+    return (
+      <div id="main-app">
+        React App!
+      </div>
+    );
+  };
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import store from "store/index";
+import App from "components/App/App";
 
 ReactDOM.render(
-  <p>React!</p>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("react-app")
 );
